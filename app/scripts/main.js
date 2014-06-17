d9581c591adfc9af87517a413d4077b6feff13f0
Joshua Smith <joshuawordsmith@gmail.com>
2014-06-17 12:34:54 -0400

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,37 @@
+
+Parse.initialize("dt8DfJoVpPqTM004VdtOUEeXXqRdxBfc88fksbNN", "gsPQIKflI4ErhhNa4Ad6HNzcyhEjoYjumZLJRKcE");
+
+// var TestObject = Parse.Object.extend("TestObject");
+// var testObject = new TestObject();
+// testObject.save({foo: "bar"}).then(function(object) {
+//   alert("yay! it worked");
+// });
+
+// var first = new Post()
+
+// var query = new Parse.Query(Post)
+
+
+// $('.save-file').click(function(){
+// 	var fileUploadControl = $('.input-file')[0];
+// 	if (fileUploadControl.files.length > 0) {
+// 	  var file = fileUploadControl.files[0];
+// 	  var title = 'photo.jpg';
+// 	  var parseFile = new Parse.File(title, file);
+// 	}
+// 	parseFile.save().done(function(){
+// 		console.log('it worked!')
+// 	}).fail(function(){
+// 		console.log('oops... it failed.')
+// 	});
+
+
+// 	var image = new Parse.Object("Post");
+// 	image.set("caption", "enlightenment");
+// 	image.set("imageFile", parseFile);
+// 	image.save();
+
+
+// // var postPhoto = image.get("imageFile");
+// // $(".image-display")[0].src = postPhoto.url();
+// })
---
+++ app/scripts/model.js
@@ -0,0 +1,4 @@
+//parse model for new image from computer
+var Post = Parse.Object.extend({
+	className: 'Post'
+});
